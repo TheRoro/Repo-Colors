4ee891faafe3668d83d3a4c90d5f0c7a9781016f
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-11-27 16:29:38 -0500

    ⚡️Update New GitHub style classes

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, current_tab_info => {
-        if(/^https:\/\/www\.github/.test(current_tab_info.url)) {
-            chrome.tabs.insertCSS(null, {file: './styles.css'})
-            chrome.tabs.insertCSS(null, {file: './languages.css'})
-            chrome.tabs.executeScript(null, {file: './foreground.js'})
-        }
-    })
+chrome.tabs.onActivated.addListener((tab) => {
+	chrome.tabs.get(tab.tabId, (current_tab_info) => {
+		if (/^https:\/\/www\.github/.test(current_tab_info.url)) {
+			chrome.tabs.insertCSS(null, { file: './styles.css' })
+			chrome.tabs.insertCSS(null, { file: './languages.css' })
+			chrome.tabs.executeScript(null, { file: './foreground.js' })
+		}
+	})
 })
